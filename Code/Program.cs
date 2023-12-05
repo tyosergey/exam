2810b12eb446dcec0e899ae23f3dd666fdb6651d
tyosergey <tyosergey@gmail.com>
2023-12-05 16:15:40 +0500

    Code

---
+++ Code/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] array = {"Coding", "is", "the", "best", "!!"}; // Создаем строковый массив
+
+void ResultArray(string[] array) // Метод принимает на вход созданный нам массив
+{
+    string[] arr = new string[array.Length]; // Создаем новый массив
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) // Проверяем является ли строка меньше, или равно трем символам
+        {
+            arr[i] = array[i]; // Записываем элемент со старого массива в новый массив
+        }
+    }
+    
+    for (int j = 0; j < arr.Length; j++)
+    {
+        System.Console.Write(arr[j] + " "); // Выводим элементы нового массива
+    }
+}
+
+ResultArray(array); // Вызываем метод
